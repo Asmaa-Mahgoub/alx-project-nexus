76c6affdf4748068c8e52c7a8f70670c5ef567b7
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2026-02-08 23:53:56 +0200

    Fixing issues in allowed hosts in settings file

--- prodexa/settings.py
+++ prodexa/settings.py
@@ -33,8 +33,9 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  os.environ.get('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "alx-project-nexus-j3g2.onrender.com"]
 
-ALLOWED_HOSTS =os.environ.get('alx-project-nexus-j3g2.onrender.com', "127.0.0.1,localhost").split(",")
+#ALLOWED_HOSTS =os.environ.get('alx-project-nexus-j3g2.onrender.com', "127.0.0.1,localhost").split(",")
 #ALLOWED_HOSTS = ['alx-project-nexus-j3g2.onrender.com','localhost','127.0.0.1']
 
 # Application definition
